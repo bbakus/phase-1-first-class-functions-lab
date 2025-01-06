e77e235829a8ec3d1446413cbdec3ed92f6ca5aa
Brandon Bakus <brandonbakus@gmail.com>
2025-01-06 07:07:34 -0500

    my head hurts a little

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function(){
+        const firstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+            return firstTwoDrivers.slice(0,2)
+        
+}
+
+const returnLastTwoDrivers = function(){
+    const firstTwoDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+        return firstTwoDrivers.slice(2,4)
+    
+
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+    return function(fare){
+        return integer * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(arrayOfDrivers, whichDrivers){
+        return whichDrivers(arrayOfDrivers)
+}
+
+    selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers)
+    selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers)
